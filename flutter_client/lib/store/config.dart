d63212a1ae590a48d6b04ac1046d48b48fd9ad96
yingshaoxo <yingshaoxo@gmail.com>
2023-02-24 13:05:33 +0800

    change flutter config to fit server service

--- flutter_client/lib/store/config.dart
+++ flutter_client/lib/store/config.dart
@@ -17,21 +17,40 @@ class RoutesMap {
   static const one_to_one_chat_page = "/one_to_one_chat_page";
 }
 
+// class GrpcConfig {
+//   static const port_number = 80;
+
+//   static const whoami_service = "whoami.weloveparty.domain.local";
+//   static const account_auth_service = "auth.weloveparty.domain.local";
+//   static const account_storage_service =
+//       "account_storage.weloveparty.domain.local";
+//   static const room_control_service = "room_control.weloveparty.domain.local";
+//   static const free_map_service = "free_map.weloveparty.domain.local";
+//   static const chat_with_friends_service =
+//       "chat_with_friends.weloveparty.domain.local";
+// }
+
+// class LivekitConfig {
+//   static const url = 'ws://livekit.weloveparty.domain.local';
+// }
+
 class GrpcConfig {
   static const port_number = 80;
 
-  static const whoami_service = "whoami.weloveparty.domain.local";
-  static const account_auth_service = "auth.weloveparty.domain.local";
+  static const whoami_service = "whoami.weloveparty.ai-tools-online.xyz";
+  static const account_auth_service = "auth.weloveparty.ai-tools-online.xyz";
   static const account_storage_service =
-      "account_storage.weloveparty.domain.local";
-  static const room_control_service = "room_control.weloveparty.domain.local";
-  static const free_map_service = "free_map.weloveparty.domain.local";
+      "account_storage.weloveparty.ai-tools-online.xyz";
+  static const room_control_service =
+      "room_control.weloveparty.ai-tools-online.xyz";
+  static const free_map_service = "free_map.weloveparty.ai-tools-online.xyz";
   static const chat_with_friends_service =
-      "chat_with_friends.weloveparty.domain.local";
+      "chat_with_friends.weloveparty.ai-tools-online.xyz";
 }
 
 class LivekitConfig {
-  static const url = 'ws://livekit.weloveparty.domain.local';
+  static const url = 'ws://livekit.weloveparty.ai-tools-online.xyz';
+  // static const url = 'ws://144.202.109.163';
 }
 
 bool in_dev_mode = false;
