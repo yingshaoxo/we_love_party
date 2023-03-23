1f901c7fd3695e7884731d3ca53aa2400e3f0c3b
yingshaoxo <yingshaoxo@gmail.com>
2023-03-23 09:31:33 +0000

    I think you should do a local test first

--- email_platform/email_send.py
+++ email_platform/email_send.py
@@ -4,9 +4,10 @@ from email.message import EmailMessage
 msg = EmailMessage()
 msg['Subject'] = f'Hi, you!'
 msg['From'] = "yingshaoxo@gmail.com"
-msg['To'] = "lili@mail.weloveparty.ai-tools-online.xyz"
+msg['To'] = "god@mail.weloveparty.ai-tools-online.xyz"
 
-s = smtplib.SMTP('mail.weloveparty.ai-tools-online.xyz', port=25)
+# s = smtplib.SMTP('mail.weloveparty.ai-tools-online.xyz', port=25)
 # s = smtplib.SMTP('0.0.0.0', port=25)
+s = smtplib.SMTP('127.0.0.1', port=25)
 s.send_message(msg)
 s.quit()
