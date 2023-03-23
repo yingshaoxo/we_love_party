39207f81938017e2a0e5951ebe9fa5700667a106
yingshaoxo <yingshaoxo@gmail.com>
2023-03-23 11:49:21 +0000

    now we could get email behind the traefik, but can't get the real ip of the email sender

--- email_platform/email_send.py
+++ email_platform/email_send.py
@@ -7,6 +7,7 @@ msg['From'] = "yingshaoxo@gmail.com"
 msg['To'] = "god@mail.weloveparty.ai-tools-online.xyz"
 
 # s = smtplib.SMTP('mail.weloveparty.ai-tools-online.xyz', port=25)
-s = smtplib.SMTP('0.0.0.0', port=25)
+#s = smtplib.SMTP('0.0.0.0', port=25)
+s = smtplib.SMTP('127.0.0.1', port=25)
 s.send_message(msg)
 s.quit()
